07d5ed2d31f82d925e439fb6fb5e5f645db20261
Lee Samuel <reiil8@Lee-Samuels-MBP.telecom>
2015-10-21 20:20:08 +1300

    init

---
+++ app/config/routes.js
@@ -0,0 +1,11 @@
+// var React = require('react');
+// var Main = require('../components/Main.js');
+// var Home = require('../components/Home.js');
+// var Router = require('react-router');
+// var DefaultRoute = Router.DefaultRoute;
+// var Route = Router.Route;
+
+// module.export = (
+// 	<Route name="app" path="/" component={Main}>
+// 	</Route>
+// );
