cf1e798ba7297029ba7a6fd1d05e95d24eaca271
Lee Samuel <reiil8@Lee-Samuels-MBP.telecom>
2015-10-31 18:48:36 +1300

    data

--- app/App.js
+++ app/App.js
@@ -41,10 +41,83 @@ let App = React.createClass({
 });
 
 let Pizzerias = React.createClass({
-	render() {
-		return (
-			<h3>All the pizzerias!</h3>
+	getInitialState() {
+		return(
+			{
+				locations: [
+					{
+						type: 'Feature',
+						properties: {
+							city: "Oakland",
+							pizzeria: "Hi-Life",
+							website: "http://www.hilifeoakland.com",
+							address: "400 15th St",
+							markersize: "medium",
+							markercolor: "ffff00",
+							markersymbol: "restaurant"
+						},
+						geometry: {
+							type: "Point",
+							coordinates: [
+								-122.2694895,
+								37.805068
+							]
+						}
+					},
+					{
+						type: "Feature",
+						properties: {
+							city: "Oakland",
+							pizzeria: "Zachary's",
+							website: "http://zacharys.com/locations/oakland",
+							address: "5801 College Ave.",
+							markersize: "medium",
+							markercolor: "ffff00",
+							markersymbol: "restaurant"
+						},
+						geometry: {
+							type: "Point",
+							coordinates: [
+								-122.2521705,
+								37.8462724
+							]
+						}
+					}
+				]
+			}
 		)
+	},
+	// componentDidMount() {
+	// 	$.ajax({
+	// 		url: './api/pizzerias.json',
+	// 		type: 'get',
+	// 		dataType: 'JSON',
+	// 		success: function(data) {
+	// 			console.log(data);
+	// 			this.setState({ locations: data})
+	// 		}.bind(this)
+	// 	})
+	// },
+	render() {
+	    let pizzerias = this.state.locations.map(function(pizzeria, index) {
+	      let location = pizzeria.properties
+	      return(
+	          <div className='thumbnail' key={index}>
+	            <div className='caption'>
+	              <h5>{location.pizzeria}</h5>
+	              <a href={"https://www.google.com/maps/place/" + location.address + ' ' + location.city} target='_blank'>{location.address}</a>
+	              <p>{location.city}</p>
+	              <a href={location.website} target='_blank'>Website</a>
+	            </div>
+	          </div>
+	      )
+	    });
+	    return(
+	      <div>
+	        <h3>Pizzerias</h3>
+	          {pizzerias}
+	      </div>
+	    )
 	}
 });
 
