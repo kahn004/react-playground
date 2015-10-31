6d26c40352ac8c68ff6adf453b1c3eebb0260f8a
Lee Samuel <reiil8@Lee-Samuels-MBP.telecom>
2015-10-31 18:46:29 +1300

    data

--- public/bundle.js
+++ public/bundle.js
@@ -143,11 +143,93 @@
 	var Pizzerias = _react2['default'].createClass({
 		displayName: 'Pizzerias',
 
+		getInitialState: function getInitialState() {
+			return {
+				locations: [{
+					type: 'Feature',
+					properties: {
+						city: "Oakland",
+						pizzeria: "Hi-Life",
+						website: "http://www.hilifeoakland.com",
+						address: "400 15th St",
+						markersize: "medium",
+						markercolor: "ffff00",
+						markersymbol: "restaurant"
+					},
+					geometry: {
+						type: "Point",
+						coordinates: [-122.2694895, 37.805068]
+					}
+				}, {
+					type: "Feature",
+					properties: {
+						city: "Oakland",
+						pizzeria: "Zachary's",
+						website: "http://zacharys.com/locations/oakland",
+						address: "5801 College Ave.",
+						markersize: "medium",
+						markercolor: "ffff00",
+						markersymbol: "restaurant"
+					},
+					geometry: {
+						type: "Point",
+						coordinates: [-122.2521705, 37.8462724]
+					}
+				}]
+			};
+		},
+		// componentDidMount() {
+		// 	$.ajax({
+		// 		url: './api/pizzerias.json',
+		// 		type: 'get',
+		// 		dataType: 'JSON',
+		// 		success: function(data) {
+		// 			console.log(data);
+		// 			this.setState({ locations: data})
+		// 		}.bind(this)
+		// 	})
+		// },
 		render: function render() {
+			var pizzerias = this.state.locations.map(function (pizzeria, index) {
+				var location = pizzeria.properties;
+				return _react2['default'].createElement(
+					'div',
+					{ className: 'thumbnail', key: index },
+					_react2['default'].createElement(
+						'div',
+						{ className: 'caption' },
+						_react2['default'].createElement(
+							'h5',
+							null,
+							location.pizzeria
+						),
+						_react2['default'].createElement(
+							'a',
+							{ href: "https://www.google.com/maps/place/" + location.address + ' ' + location.city, target: '_blank' },
+							location.address
+						),
+						_react2['default'].createElement(
+							'p',
+							null,
+							location.city
+						),
+						_react2['default'].createElement(
+							'a',
+							{ href: location.website, target: '_blank' },
+							'Website'
+						)
+					)
+				);
+			});
 			return _react2['default'].createElement(
-				'h3',
+				'div',
 				null,
-				'All the pizzerias!'
+				_react2['default'].createElement(
+					'h3',
+					null,
+					'Pizzerias'
+				),
+				pizzerias
 			);
 		}
 	});
